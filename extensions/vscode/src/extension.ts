3c8409698e41fb3f50172687d59f078217a5774a
Stefan Legien <stefan@legien.org>
2016-01-31 20:35:02 +0100

    Basis-Struktur für VSCode Extension generiert

---
+++ extensions/vscode/src/extension.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode'; 
+
+export function activate(context: vscode.ExtensionContext) {
+    // TODO: Start backend
+}
+
+export function deactivate() {
+    // TODO: Stop backend 
+}
