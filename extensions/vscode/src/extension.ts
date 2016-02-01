4e36adc41fcf14b59237478cd4bbdc4f1a452dce
Stefan Legien <stefan@legien.org>
2016-02-01 20:09:37 +0100

    Festes template für Keyword "dummy" implementiert.
    Resolves issue #2

--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -1,7 +1,11 @@
-import * as vscode from 'vscode'; 
+import * as completion from './completion/completion';
+import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
     // TODO: Start backend
+    var liveTemplateItemProvider = new completion.LiveTemplateItemProvider();
+    var disposable = vscode.languages.registerCompletionItemProvider('*', liveTemplateItemProvider, '*');
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {
